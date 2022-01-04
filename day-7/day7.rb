8e4f00c2042202ff2c713d671c1937c92fdc1eaf
Gonzo <gonzonieto@gmail.com>
2022-01-03 19:43:00 -0500

    day 7 part 2 solved

--- day-7/day7.rb
+++ day-7/day7.rb
@@ -6,9 +6,12 @@ dist_ary = Array.new(crabs.max+1)
 # For each distance, calculate the total fuel required to move all crabs there
 dist_ary.each_with_index do |x, i|
 
-	# Each distance is the difference between the crab's starting position
+	# First, find the difference between the crab's starting position
 	# and the current position being evaluated
- 	dist_ary[i] = crabs.map{|e| (e-i).abs}.sum
+	
+	# Second, add one to that number, then divide it by two
+	# and multiply it by itself to find fuel needed for travel
+ 	dist_ary[i] = crabs.map{|e| n = (e-i).abs; n * (n+1)/2}.sum
 end
 
 puts dist_ary.min
